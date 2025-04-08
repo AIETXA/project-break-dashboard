ceda5d80e691991987abfb18dc817e20d07ac11d
Ailén <ailenechaguibel@gmail.com>
2025-04-08 13:03:18 +0200

    imagenes cargadas

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('funciona');
+const imagenesFondo = [img/1.jpg, img/2.jpg, IMG/3.JPG, IMG\4.JPG, IMG\5.JPG, IMG\6.JPG, IMG\7.JPG, IMG\8.JPG, IMG\9.JPG, IMG\10.JPG, IMG\11.JPG, IMG\12.JPG, IMG\13.JPG, IMG\14.JPG, IMG\15.JPG, IMG\16.JPG, IMG\17.JPG, IMG\18.JPG, IMG\19.JPG, IMG\20.JPG, IMG\21.JPG, IMG\22.JPG, IMG\23.JPG, IMG\24.JPG, IMG\25.JPG]
+console.log(0);
+
+
+//`document.body.style.backgroundImage = "url('mi-imagen.jpg')"`
