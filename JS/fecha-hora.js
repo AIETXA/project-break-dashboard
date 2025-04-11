f581e07f94494dd451904b8051211a273012844d
Ailén <ailenechaguibel@gmail.com>
2025-04-11 10:46:11 +0200

    reloj funcionando ok con fecha ok

--- JS/fecha-hora.js
+++ JS/fecha-hora.js
@@ -1,15 +1,61 @@
-const hoy = document.querySelector('fecha-hora');
+const hoy = document.querySelector('fecha');
+const cajaSegundosAmPm = document.querySelector('caja-segundos-ampm');
 const lugar = document.getElementById('zona-horaria');
-//let relojOk = hour + ':' + minutes + ':' + seconds
-function horarioActual() {
-    const momentoActual = new Date();
-    hour = momentoActual.getHours(),
-    minutes = momentoActual.getMinutes(),
-    seconds = momentoActual.getSeconds(),
+
+
+function actualizarReloj() {
+let momentoActual = new Date();
+    hora = momentoActual.getHours(),
+    minutos = momentoActual.getMinutes(),
+    segundos = momentoActual.getSeconds(),
+    dia = momentoActual.getDay();
+    numeros = momentoActual.getDate();
+    mes = momentoActual.getMonth();
+    año = momentoActual.getFullYear();
+    ampm = '';
     
-    hoy.innerHTML 
+    pDia = document.getElementById('dia');
+    pNumero = document.getElementById('numero');
+    pMes = document.getElementById('mes');
+    pAño = document.getElementById('año');
+    pHoras = document.getElementById('horas');
+    pMinutos = document.getElementById('minutos');
+    pAMPM = document.getElementById('am-pm');
+    pSegundos = document.getElementById('segundos');
+    
+const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado'];
+pDia.textContent = diasSemana[dia]; 
+
+pNumero.textContent = numeros;
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+pMes.textContent = meses[mes];
+pAño.textContent = año;
+
+if (hora >= 12) {
+    hora = hora - 12;
+    ampm = 'PM';
+} else {
+    ampm = 'AM';
 }
 
-setInterval(horarioActual,1000);
+if (hora == 0) {
+    hora = 12;
+};
+pHoras.textContent = hora;
+pAMPM.textContent = ampm;
 
-horarioActual()
+if (minutos < 10) {
+    minutos = '0' + minutos
+};
+if (segundos < 10) {
+    segundos = '0' + segundos
+};
+
+pMinutos.textContent = minutos;
+pSegundos.textContent = segundos;
+ 
+};
+
+actualizarReloj();
+setInterval(actualizarReloj, 1000);
