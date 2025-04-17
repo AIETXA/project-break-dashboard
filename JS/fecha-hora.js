c18b13013e39a5c647d1525652b7125133837da8
Ailén <ailenechaguibel@gmail.com>
2025-04-17 10:04:46 +0200

    fecha hora y password terminadas

--- JS/fecha-hora.js
+++ JS/fecha-hora.js
@@ -2,6 +2,28 @@ const hoy = document.querySelector('fecha');
 const cajaSegundosAmPm = document.querySelector('caja-segundos-ampm');
 const lugar = document.getElementById('zona-horaria');
 
+const imagenesFondo = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/6.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg', 'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg', 'img/17.jpg', 'img/18.jpg', 'img/19.jpg', 'img/20.jpg', 'img/21.jpg', 'img/22.jpg', 'img/23.jpg', 'img/24.jpg', 'img/25.jpg'];
+
+let indiceInicial = 5;
+function imagenesInicio() {
+   document.body.style.backgroundImage = `url('${imagenesFondo[indiceInicial]}')`;
+indiceInicial = (indiceInicial + 1) % imagenesFondo.length;
+}
+
+setInterval(imagenesInicio, 5000);
+
+imagenesInicio();
+
+const fechaYhora = document.getElementById('fecha-hora');
+
+function imgRandom() {
+    fechaYhora.addEventListener('click', () => {
+
+      
+        const pagina = Math.floor(Math.random() * imagenesFondo.length);
+        document.body.style.backgroundImage = `url('${imagenesFondo[pagina]}')`;
+    })
+}
 
 function actualizarReloj() {
 let momentoActual = new Date();
--- JS/password.js
+++ JS/password.js
@@ -10,7 +10,28 @@ let checkbox2 = document.getElementById("checkbox2");
 let contraseñaGenerada = document.getElementById("contraseña-generada");
 const btnGenerador = document.getElementById("btn-generador");
 
+const imagenesFondo = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/6.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg', 'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg', 'img/17.jpg', 'img/18.jpg', 'img/19.jpg', 'img/20.jpg', 'img/21.jpg', 'img/22.jpg', 'img/23.jpg', 'img/24.jpg', 'img/25.jpg'];
 
+let indiceInicial = 8;
+function imagenesInicio() {
+   document.body.style.backgroundImage = `url('${imagenesFondo[indiceInicial]}')`;
+indiceInicial = (indiceInicial + 1) % imagenesFondo.length;
+}
+
+setInterval(imagenesInicio, 5000);
+
+imagenesInicio();
+
+const fechaYhora = document.getElementById('fecha-hora');
+
+function imgRandom() {
+    fechaYhora.addEventListener('click', () => {
+
+      
+        const pagina = Math.floor(Math.random() * imagenesFondo.length);
+        document.body.style.backgroundImage = `url('${imagenesFondo[pagina]}')`;
+    })
+}
 
 const generarContraseña = (caracteres, length) => {
     let contraseña = "";
